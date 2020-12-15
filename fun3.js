252f591a4c5a5d177f32ba2fbe5a90c66fd139e7
Tokonigeorge <65655487+Tokonigeorge@users.noreply.github.com>
2020-12-15 17:25:46 +0100

    Update fun3.js

--- fun3.js
+++ fun3.js
@@ -1,16 +1,4 @@
-//const menuBtn = document.querySelector('.menu-btn');
-//let menuOpen = false;
-//menuBtn.addEventListener('click', () => {
-  //  if(!menuOpen) {
-    //    menuBtn.classList.add('open');
-      //  menuOpen = true;
-    //} else{
-      //  menuBtn.classList.remove('open');
-        //menuOpen = false7
-    //}
-//}
 
-//);
 
 (function () {
  var button = document.getElementById('toggle-menu');
